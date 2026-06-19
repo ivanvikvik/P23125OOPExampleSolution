942d3d4a06893eb13cd6f7b588e978f471d53064
Victor Ivanchenko <ivanvikvik@gmail.com>
2026-06-19 19:14:44 +0300

    Use constructors and destructor

--- OOPTestProject/Student.h
+++ OOPTestProject/Student.h
@@ -7,7 +7,94 @@ class Student {
 public:
 	string name;
 	int age;
-	double mark;
+	//double mark;
 	char gender;	
 	bool alive;
+	int* marks;
+	int count;
+
+	// default-constructor
+	Student() {
+		cout << "Student default constructor\n";
+		name = "";
+		age = 0;
+		alive = false;
+		//mark = 0;
+		marks = NULL;
+		count = 0;
+		gender = '\0';
+	}
+
+	Student(string nm, int ag) {
+		cout << "Student constructor with arguments\n";
+		name = nm;
+		age = ag;
+		alive = false;
+		//mark = 0;
+		marks = NULL;
+		count = 0;
+		gender = '\0';
+	}
+
+	Student(int ag, string nm) {
+		cout << "Student constructor with arguments\n";
+		name = nm;
+		age = ag;
+		alive = false;
+		//mark = 0;
+		marks = NULL;
+		count = 0;
+
+		gender = '\0';
+	}
+
+	// canonical constructor
+	Student(string nm, int ag, char gndr, bool a, int* mrks, int cnt) {
+		cout << "Student canonical constructor\n";
+		name = nm;
+		age = ag;
+		alive = a;
+		//mark = mrk;
+		marks = mrks;
+		count = cnt;
+		gender = gndr;
+	}
+
+	// copy-constructor
+	Student(const Student* student) {
+		cout << "Student copy-constructor\n";
+		name = student->name;
+		age = student->age;
+		alive = student->alive;
+		//mark = student->mark;
+		marks = student->marks;
+		count = student->count;
+		gender = student->gender;
+	}
+
+	// destructor
+	~Student() {
+		cout << "Student destructor\n";
+		if (marks != nullptr) {
+			delete[] marks;
+			count = 0;
+		}
+	}
+
+	string toString() {
+		string s = "";
+
+		string mrks = "";
+
+		for (int i = 0; i < count; i++)
+		{
+			mrks += to_string(*(marks + i)) + " ";
+		}
+
+		s += "Student: " + name + ", age = " + to_string(age)
+			+ ", gender = " + gender + ", mark = " + mrks
+			+ ", alive = " + (alive ? "yes" : "no");
+
+		return s;
+	}
 };
